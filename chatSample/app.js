ba080f8f540335dd32cf873201d5abf1db329498
astics <nikunjblu.tandel@gmail.com>
2017-11-15 12:32:41 +0530

    listen and set port code added for chatSample

--- chatSample/app.js
+++ chatSample/app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port',process.env.PORT || 7000);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -41,6 +42,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(app.get('port'),()=>{
+    console.log("welcome to new Project");
 });
 
 module.exports = app;
